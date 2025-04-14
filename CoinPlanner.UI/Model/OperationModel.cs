18739fe1ab1fdcd01981d1a252a62e04cd22d655
Vladislav <Vladislav@ASPIRE3>
2025-04-14 17:57:14 +0300

    [Add] Добавлены модели данных

---
+++ CoinPlanner.UI/Model/OperationModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinPlanner.UI.Model;
+
+public class OperationModel
+{
+    public int OperId { get; set; }
+    public int OperPlanId { get; set; }
+    public string? OperName { get; set; }
+    public int OperType_Id { get; set; }
+    public double OperSum { get; set; }
+    public bool OperCompleted { get; set; }
+    public DateTime OperNextDate { get; set; }
+}
---
+++ CoinPlanner.UI/Model/PlanModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinPlanner.UI.Model;
+
+public class PlanModel
+{
+    public int PlanId { get; set; }
+    public string? PlanName { get; set; }
+    public DateTime DataCreate { get; set; }
+    public DateTime DataUpdate { get; set; }
+    public List<OperationModel>? OperationModels { get; set; }
+}
